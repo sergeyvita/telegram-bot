c5c409c408f3d2469bce4511ad04aaf26fabad10
sergeyvita <s.shaglin@gmail.com>
2024-12-15 17:11:21 +0300

    изменение алгоритма порта

--- main.py
+++ main.py
@@ -79,5 +79,7 @@ def send_message(chat_id, text):
     except Exception as e:
         print(f"Ошибка при отправке сообщения: {e}")  # Отладочные логи
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+from waitress import serve
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
